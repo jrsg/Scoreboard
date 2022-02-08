1e3010f9aa538ad2a8168596cb6b8996ae84268b
Rubén Sánchez <rsanchez@ucc.mx>
2022-02-08 14:05:21 -0600

    Commit inicial

---
+++ app/src/main/java/mx/ucc/scoreboard/MainActivity.java
@@ -0,0 +1,14 @@
+package mx.ucc.scoreboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
